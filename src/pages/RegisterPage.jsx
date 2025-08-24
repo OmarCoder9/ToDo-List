9145bad15f87128286f2c9118e2723971530c44e
Omar Ahmed <omar.ahmed9806@gmail.com>
2025-08-24 22:40:56 +0300

    added sign up page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Register from '../components/register/Register'
+import SidePic from '../components/shared/sidePic/SidePic'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className="grid grid-cols-2 gap-6 min-h-screen bg-secondary-body-color py-14 px-12">
+      {/* Left Section - Visual Background */}
+      <div className="flex items-center justify-center">
+        <SidePic/>
+      </div>
+      {/* Right Section - UI Panel */}
+      <div className="flex items-center">
+        <Register/>
+      </div>
+    </div>
   )
 }
 
