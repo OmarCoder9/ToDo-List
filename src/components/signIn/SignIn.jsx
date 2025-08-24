b6794f30f0804f221e188db7b6d8f72c38bac2a0
Omar Ahmed <omar.ahmed9806@gmail.com>
2025-08-24 11:57:28 +0300

    feat: update GettingStarted and SignIn components to use React Router for navigation; improve layout and styling

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -3,6 +3,7 @@ import Joi from "joi";
 import { LuEye, LuEyeClosed } from "react-icons/lu";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
+import { Link } from "react-router";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -32,36 +33,39 @@ const SignIn = () => {
       <h1 className="font-main-font text-4xl font-bold text-black text-left mb-8 pr-36">
         Sign in
       </h1>
-    <div className="">
-      <input
-        type="text"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        placeholder="mail.example@mail.com"
-        className="input-field"
-      />
-      <div className="relative">
+      <div className="">
         <input
-          type={showPassword ? "text" : "password"}
-          value={password}
-          onChange={handleChange}
-          placeholder="**************"
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="mail.example@mail.com"
           className="input-field"
         />
-        <div className="absolute left-53 pl-4 top-1.5">
-          <button onClick={() => setShowPassword(!showPassword)}>
-            {showPassword ? <LuEyeClosed /> : <LuEye />}
-          </button>
+        <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={handleChange}
+            placeholder="**************"
+            className="input-field"
+          />
+          <div className="absolute left-53 pl-4 top-1.5">
+            <button onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <LuEyeClosed /> : <LuEye />}
+            </button>
+          </div>
+          {error && <p className="text-red-600">{error}</p>}
+          {!error && password && (
+            <p className="text-green-500">Password is valid</p>
+          )}
         </div>
-        {error && <p className="text-red-600">{error}</p>}
-        {!error && password && (
-          <p className="text-green-500">Password is valid</p>
-        )}
-      </div>
-      
+
         {/* Sign In Button */}
-        <button type="submit" className="w-full py-3 mb-6 text-white bg-btn-gr rounded-2xl font-semibold hover:bg-btn-gr-hover transition">
-          Sign In
+        <button
+          type="submit"
+          className="w-full py-3 mb-6 text-white bg-btn-gr rounded-2xl font-semibold hover:bg-btn-gr-hover transition"
+        >
+          <Link to={"/home"}>Sign In</Link>
         </button>
 
         {/* Divider with "or" */}
@@ -73,14 +77,12 @@ const SignIn = () => {
 
         {/* Social Buttons */}
         <div className="flex justify-center gap-4 mb-6">
-          <button className="flex items-center gap-2 px-4 py-2 bg-gray-100 rounded-lg hover:bg-gray-200 transition">
-            {/* <img src="https://www.svgrepo.com/show/355037/google.svg" alt="Google" className="w-5 h-5" /> */}
-            <FcGoogle size={20}/>
+          <button className="flex items-center gap-2 px-4 py-2 cursor-pointer bg-gray-100 rounded-lg hover:bg-gray-200 transition">
+            <FcGoogle size={20} />
             Google
           </button>
-          <button className="flex items-center gap-2 px-4 py-2 bg-gray-100 rounded-lg hover:bg-gray-200 transition">
-            {/* <img src="https://www.svgrepo.com/show/349523/facebook.svg" alt="Facebook" className="w-5 h-5" /> */}
-            <FaFacebook size={18}/>
+          <button className="flex items-center gap-2 px-4 py-2 cursor-pointer bg-gray-100 rounded-lg hover:bg-gray-200 transition">
+            <FaFacebook size={18} />
             Facebook
           </button>
         </div>
@@ -88,12 +90,14 @@ const SignIn = () => {
         {/* Sign Up Link */}
         <p className="text-center text-gray-600">
           Don’t have an account?{" "}
-          <button className="font-semibold text-black hover:underline" onClick={() => window.location.href = "/register"}>
+          <Link
+            to={"/register"}
+            className="font-semibold text-black hover:underline"
+          >
             Sign up
-          </button>
+          </Link>
         </p>
-    
-    </div>
+      </div>
     </div>
   );
 };
